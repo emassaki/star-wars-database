dfb48e1d09b4482614551ec5a6e9d5528c8d59b4
emassaki <massakihirayama@gmail.com>
2021-03-23 14:23:45 -0300

    Updates

---
+++ precache-manifest.de90966062e2876fe73cee676cd0b2bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac01e91583ee7717134fa12761959ec",
+    "url": "/star-wars-database/index.html"
+  },
+  {
+    "revision": "8a7fcd61ddfd15889501",
+    "url": "/star-wars-database/static/css/main.c8e09488.chunk.css"
+  },
+  {
+    "revision": "7655cc91b92c9d77cfb5",
+    "url": "/star-wars-database/static/js/2.a408e622.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/star-wars-database/static/js/2.a408e622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a7fcd61ddfd15889501",
+    "url": "/star-wars-database/static/js/main.c10b6f10.chunk.js"
+  },
+  {
+    "revision": "78eeeff4a11c39ce078c",
+    "url": "/star-wars-database/static/js/runtime-main.3cbd7c62.js"
+  }
+]);
